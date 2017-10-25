8c9114853a78e986ce810e5fcc869af57dda8371
L Karthik <l.karthik@ericsson.com>
2017-10-25 23:01:50 +0530

    standalone dockerfile first draft

--- vanilla_rails/app/controllers/site_controller.rb
+++ vanilla_rails/app/controllers/site_controller.rb
@@ -1,8 +1,9 @@
 class SiteController < ApplicationController
   def index
-    page_count = PageCount.find_or_create_by(id: 1)
-    page_count.count += 1
-    page_count.save!
-    @count = page_count.count
+    # page_count = PageCount.find_or_create_by(id: 1)
+    # page_count.count += 1
+    # page_count.save!
+    # @count = page_count.count
+    @count = rand.to_s[2..3]
   end
 end
