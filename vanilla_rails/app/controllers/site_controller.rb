27dd9f696a0365372dc5aa8d476ee4b5c2daa0a0
Karthik L <karthikonly@gmail.com>
2017-11-21 20:59:15 -0500

    deis version

--- vanilla_rails/app/controllers/site_controller.rb
+++ vanilla_rails/app/controllers/site_controller.rb
@@ -1,14 +1,14 @@
 class SiteController < ApplicationController
   def index
-    page_count = PageCount.find_or_create_by(id: 1)
-    page_count.count += 1
-    page_count.save!
-    @postgres_count = page_count.count
+    # page_count = PageCount.find_or_create_by(id: 1)
+    # page_count.count += 1
+    # page_count.save!
+    # @postgres_count = page_count.count
 
     @random_count = rand.to_s[2..3]
 
-    @redis_count = $redis.get('redis_count').to_i
-    @redis_count += 1
-    $redis.set('redis_count', @redis_count)
+    # @redis_count = $redis.get('redis_count').to_i
+    # @redis_count += 1
+    # $redis.set('redis_count', @redis_count)
   end
 end
